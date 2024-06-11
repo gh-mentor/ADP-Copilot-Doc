6021cb5094370e24d762ce4f8451efbd5e7f4649
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-06-11 07:26:46 -0400

    update

--- PayrollLibrary/HourlyEmployee.cs
+++ PayrollLibrary/HourlyEmployee.cs
@@ -5,31 +5,42 @@
         private double _HourlyRate;
         private double _HoursWorked;
 
-        public double HourlyRate { get { return _HourlyRate; } set { _HourlyRate = value; } }
-        public double HoursWorked { get { return _HoursWorked; } set { _HoursWorked = value; } }
+        _HourlyRate = value; }
+}
+public double HoursWorked
+{
+    get { return _HoursWorked; }
+    set
+    {
+        if (value < 0)
+        {
+            throw new ArgumentException("Hours worked cannot be negative.");
+        }
+        _HoursWorked = value;
+    }
+}
 
-
-        public HourlyEmployee(int? id, int? reportsTo, string? name, string? email, string? mobile, int? departmentId, double hourlyRate = 0, double hoursWorked = 0)
+public HourlyEmployee(int? id, int? reportsTo, string? name, string? email, string? mobile, int? departmentId, double hourlyRate = 0, double hoursWorked = 0)
             : base(id, reportsTo, name, email, mobile, departmentId)
         {
-            _HourlyRate = hourlyRate;
-            _HoursWorked = hoursWorked;
-        }
+    _HourlyRate = hourlyRate;
+    _HoursWorked = hoursWorked;
+}
 
-        // Add a default constructor
-        public HourlyEmployee() : base(null, null, null, null, null, null) { }
+// Add a default constructor
+public HourlyEmployee() : base(null, null, null, null, null, null) { }
 
-        public override double Payment { get { return _HourlyRate * _HoursWorked; } }
+public override double Payment { get { return _HourlyRate * _HoursWorked; } }
 
 
-        // override the EmployeeDetails property
-        public override string EmployeeDetails
-        {
-            get
-            {
-                return base.EmployeeDetails + $", Hourly Rate: {_HourlyRate}, Hours Worked: {_HoursWorked}";
-            }
-        }
+// override the EmployeeDetails property
+public override string EmployeeDetails
+{
+    get
+    {
+        return base.EmployeeDetails + $", Hourly Rate: {_HourlyRate}, Hours Worked: {_HoursWorked}";
+    }
+}
 
     }
 }
